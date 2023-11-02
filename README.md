5c92f1c580193313e4ea3b02f28f54253add10a1
sirius524 <captainhung096@gmail.com>
2023-11-02 13:20:05 +0800

    feat: support pve 3~8

--- README.md
+++ README.md
@@ -1,28 +1,14 @@
-# ProxmoxVE_Upgrade
+# Proxmox_VE-Upgrade-Script
 ![](https://www.proxmox.com/images/proxmox/logos/mediakit-proxmox-server-solutions-logos-dark.svg)
 ---
-
-### The script can solve the problem that ProxmoxVE can not update the source list
-
+### The script can solve the problem that Proxmox VE can not update the source list
 ### 解決 ProxmoxVE 無法更新的問題
 ```
 Problem: TASK ERROR: command 'apt-get update' failed: exit code 100
 ```
 <img width="323" alt="image" src="https://github.com/kevinjone25/ProxmoxVE_Upgrade/assets/64208218/33a08288-49f5-4080-9680-5e150ddb5426">
 
-
-
 &nbsp;
-
-- Using Enviroment : 8.x
-
-- 使用的 PVE 環境 : 8.x
-
-&nbsp;
-
->If the version is below or higher than 7.x please refer to PVE document(you must change Release Key and Download link according to PVE version)
+#### Reference
   - [Renew Source List Step](https://pve.proxmox.com/wiki/Install_Proxmox_VE_on_Debian_Stretch)
   - [Proxmox Repository](https://pve.proxmox.com/wiki/Package_Repositories)
->如果你的版本高於或低於 7.x 請參照Proxmox 官方文件來更新(你需要依據 PVE 版本替換 Release key 和 Download 的連結)
-  - [更新版本步驟](https://pve.proxmox.com/wiki/Install_Proxmox_VE_on_Debian_Stretch)
-  - [Proxmox 套件版本](https://pve.proxmox.com/wiki/Package_Repositories)
--- upgrade.sh
+++ upgrade.sh
@@ -6,14 +6,44 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# Choose PVE version
+echo -n "What is your PVE version(3/4/5/6/7/8):"
+read pve_version
+
+case $pve_version in
+3)
+    debian_version="wheezy"
+    ;;
+4)
+    debian_version="jessie"
+    ;;
+5)
+    debian_version="stretch"
+    ;;
+6)
+    debian_version="buster"
+    ;;
+7)
+    debian_version="bullseye"
+    ;;
+8)
+    debian_version="bookworm"
+    ;;
+*)
+    echo "You have not chosen a valid PVE version."
+    exit 1
+    ;;
+esac
+echo "You have chosen PVE ${pve_version}.*, based on Debian ${debian_version}"
+
 # Delete enterprise repo
 rm -f /etc/apt/sources.list.d/pve-enterprise.list
 
 # Add 7.x repo into update list
-echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" >>/etc/apt/sources.list.d/pve-install-repo.list
+echo "deb http://download.proxmox.com/debian/pve ${debian_version} pve-no-subscription" >>/etc/apt/sources.list.d/pve-install-repo.list
 
 # Get release key
-wget -q http://download.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+wget -q http://download.proxmox.com/debian/proxmox-release-${debian_version}.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-${debian_version}.gpg
 
 # Update & Upgrade
 apt update && apt dist-upgrade -y
